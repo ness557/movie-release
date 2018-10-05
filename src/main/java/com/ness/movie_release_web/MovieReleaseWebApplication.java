0249a2a1eb2bc2b7aeb41c31de873798a48d46ad
Roman <bezruchkor1@gmail.com>
2018-10-05 20:03:56 +0300

    Test

--- src/main/java/com/ness/movie_release_web/MovieReleaseWebApplication.java
+++ src/main/java/com/ness/movie_release_web/MovieReleaseWebApplication.java
@@ -1,12 +1,22 @@
 package com.ness.movie_release_web;
 
+import com.ness.movie_release_web.service.FilmService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MovieReleaseWebApplication {
 
+    private static Logger logger = LoggerFactory.getLogger("====>>> TEST");
+
     public static void main(String[] args) {
-        SpringApplication.run(MovieReleaseWebApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(MovieReleaseWebApplication.class, args);
+//        NotificationServiceImpl service = context.getBean(NotificationServiceImpl.class);
+        FilmService service = context.getBean(FilmService.class);
+        logger.error(service.getAll().toString());
     }
 }
