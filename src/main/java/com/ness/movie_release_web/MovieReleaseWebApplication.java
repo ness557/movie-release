ed211762ffcea9c5a2e995c38b0a4f4447974c45
Roman <bezruchkor1@gmail.com>
2018-10-25 12:00:56 +0300

    Revert "security fix (password encryption)"
    This reverts commit c3ded3a

--- src/main/java/com/ness/movie_release_web/MovieReleaseWebApplication.java
+++ src/main/java/com/ness/movie_release_web/MovieReleaseWebApplication.java
@@ -1,5 +1,6 @@
 package com.ness.movie_release_web;
 
+import com.ness.movie_release_web.service.TmdbDatesService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,6 +10,6 @@ public class MovieReleaseWebApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(MovieReleaseWebApplication.class, args);
 
-//        System.out.println(context.getBean(TmdbDatesService.class).getReleaseDates("tt1392190"));
+        System.out.println(context.getBean(TmdbDatesService.class).getReleaseDates("tt1392190"));
     }
 }
