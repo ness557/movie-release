3d8ecc251ff63582444074fd13ee8d7139da3092
Roman <bezruchkor1@gmail.com>
2018-11-05 19:48:06 +0200

    todo added

---
+++ src/main/java/com/ness/movie_release_web/service/TVSeriesService.java
@@ -0,0 +1,9 @@
+package com.ness.movie_release_web.service;
+
+public interface TVSeriesService {
+
+//    TODO
+//    (u) episode watched or not
+//    (u) current season and episode
+//    (u) tottal time spent to tv series
+}
