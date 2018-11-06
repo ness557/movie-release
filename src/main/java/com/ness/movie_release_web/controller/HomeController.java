96dbb7edc7a6de0ce478a47fb6daaf4a4406eb40
Roman <bezruchkor1@gmail.com>
2018-11-06 17:03:52 +0200

    mode change BE added

--- src/main/java/com/ness/movie_release_web/controller/HomeController.java
+++ src/main/java/com/ness/movie_release_web/controller/HomeController.java
@@ -1,11 +1,18 @@
 package com.ness.movie_release_web.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
+import java.util.Arrays;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
@@ -27,5 +34,21 @@ public class HomeController {
 
         model.addAttribute("telegram", telegramBot);
         return "home";
+    }
+
+    @PostMapping("/setMode")
+    public ResponseEntity setMode(HttpServletRequest request, HttpServletResponse response){
+
+        Optional<Cookie> first = Arrays.stream(request.getCookies()).filter(e -> "mode".equals(e.getName())).findFirst();
+
+        Cookie cookie = first.orElse(new Cookie("mode", Mode.movie.name()));
+        response.addCookie(cookie);
+
+        return ResponseEntity.ok().build();
+    }
+
+    enum Mode{
+        series,
+        movie
     }
 }
