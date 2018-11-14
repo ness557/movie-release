19e60133d2372a3d6c7b5d172377b71d6010b5b8
Roman <bezruchkor1@gmail.com>
2018-11-14 20:58:41 +0200

    model and repository sorting methods

--- src/main/java/com/ness/movie_release_web/model/TVSeries.java
+++ src/main/java/com/ness/movie_release_web/model/TVSeries.java
@@ -1,11 +1,10 @@
 package com.ness.movie_release_web.model;
 
+import com.ness.movie_release_web.model.wrapper.tmdb.tvSeries.details.Status;
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "tv_series")
@@ -20,4 +19,26 @@ public class TVSeries {
     @Id
     @Column(name = "tmdb_id")
     private Long id;
+
+    @Column(name = "name_en")
+    private String nameEn;
+
+    @Column(name = "name_ru")
+    private String nameRu;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    @Column(name = "last_episode_air_date")
+    private LocalDate lastEpisodeAirDate;
+
+    @Column(name = "vote_average")
+    private Float voteAverage;
+
+    @Enumerated
+    private Status status;
+
+    public TVSeries(Long id){
+        this.id = id;
+    }
 }
--- src/main/java/com/ness/movie_release_web/repository/UserTVSeriesRepository.java
+++ src/main/java/com/ness/movie_release_web/repository/UserTVSeriesRepository.java
@@ -3,16 +3,31 @@ package com.ness.movie_release_web.repository;
 import com.ness.movie_release_web.model.User;
 import com.ness.movie_release_web.model.UserTVSeries;
 import com.ness.movie_release_web.model.UserTVSeriesPK;
+import com.ness.movie_release_web.model.wrapper.tmdb.tvSeries.details.Status;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
 public interface UserTVSeriesRepository extends JpaRepository<UserTVSeries, UserTVSeriesPK> {
 
     Page<UserTVSeries> findAllByUserOrderByTvSeriesIdDesc(User user, Pageable pageable);
+
+    Page<UserTVSeries> findAllByUserOrderByTvSeriesNameEnDesc(User user, Pageable pageable);
+    Page<UserTVSeries> findAllByUserOrderByTvSeriesNameEnAsc(User user, Pageable pageable);
+    Page<UserTVSeries> findAllByUserOrderByTvSeriesNameRuDesc(User user, Pageable pageable);
+    Page<UserTVSeries> findAllByUserOrderByTvSeriesNameRuAsc(User user, Pageable pageable);
+    Page<UserTVSeries> findAllByUserOrderByTvSeriesReleaseDateDesc(User user, Pageable pageable);
+    Page<UserTVSeries> findAllByUserOrderByTvSeriesReleaseDateAsc(User user, Pageable pageable);
+    Page<UserTVSeries> findAllByUserOrderByTvSeriesLastEpisodeAirDateDesc(User user, Pageable pageable);
+    Page<UserTVSeries> findAllByUserOrderByTvSeriesLastEpisodeAirDateAsc(User user, Pageable pageable);
+    Page<UserTVSeries> findAllByUserOrderByTvSeriesVoteAverageDesc(User user, Pageable pageable);
+    Page<UserTVSeries> findAllByUserOrderByTvSeriesVoteAverageAsc(User user, Pageable pageable);
+
+    Page<UserTVSeries> findAllByUserAndTvSeriesStatus(User user, List<Status> statuses, Pageable pageable);
 }
