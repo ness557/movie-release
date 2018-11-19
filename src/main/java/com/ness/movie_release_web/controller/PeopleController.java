3f8e204dc7c929f3fde3d493914e773e65c2ed1e
Roman <bezruchkor1@gmail.com>
2018-11-19 11:21:41 +0200

    person page added

--- src/main/java/com/ness/movie_release_web/controller/PeopleController.java
+++ src/main/java/com/ness/movie_release_web/controller/PeopleController.java
@@ -1,23 +1,58 @@
 package com.ness.movie_release_web.controller;
 
+import com.ness.movie_release_web.model.User;
+import com.ness.movie_release_web.model.wrapper.tmdb.Language;
+import com.ness.movie_release_web.model.wrapper.tmdb.Mode;
+import com.ness.movie_release_web.model.wrapper.tmdb.people.PeopleWrapper;
+import com.ness.movie_release_web.service.UserService;
 import com.ness.movie_release_web.service.tmdb.PeopleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
     @Autowired
+    private UserService userService;
+
+    @Autowired
     private PeopleService peopleService;
 
     @GetMapping("/search")
-    public ResponseEntity search(@RequestParam(value = "query") String query){
+    public ResponseEntity search(@RequestParam(value = "query") String query) {
 
         return ResponseEntity.ok(peopleService.search(query));
+    }
+
+    @GetMapping("/{tmdbId}")
+    public String getDetails(@PathVariable("tmdbId") Integer id,
+                             Model model,
+                             Principal principal) {
+
+        User user = userService.findByLogin(principal.getName());
+        Language language = user.getLanguage();
+        Mode mode = user.getMode();
+        model.addAttribute("language", language);
+        model.addAttribute("mode", mode);
+
+        Optional<PeopleWrapper> peopleOpt = peopleService.getDetails(id, language);
+        if (!peopleOpt.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        model.addAttribute("person", peopleOpt.get());
+
+        return "personInfo";
     }
 }
