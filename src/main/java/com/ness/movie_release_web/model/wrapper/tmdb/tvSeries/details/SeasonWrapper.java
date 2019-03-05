fc9bd6b6c6479e09282d42aba5367ba61fdf2dde
Roman <ness55779@gmail.com>
2019-03-05 11:03:12 +0200

    Fix ToString annotation. New pagination buttons logic. Now can be clicked all over the block

--- src/main/java/com/ness/movie_release_web/model/wrapper/tmdb/tvSeries/details/SeasonWrapper.java
+++ src/main/java/com/ness/movie_release_web/model/wrapper/tmdb/tvSeries/details/SeasonWrapper.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString(exclude = "episodes", "overview")
+@ToString(exclude = {"episodes", "overview"})
 @EqualsAndHashCode
 public class SeasonWrapper {
 
