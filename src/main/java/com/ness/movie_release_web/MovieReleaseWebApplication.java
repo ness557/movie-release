dd5465d362a5affaeb412807864fb228ae69a735
Roman <ness55779@gmail.com>
2019-09-09 19:38:19 +0300

    fix cache speed

--- src/main/java/com/ness/movie_release_web/MovieReleaseWebApplication.java
+++ src/main/java/com/ness/movie_release_web/MovieReleaseWebApplication.java
@@ -2,6 +2,7 @@ package com.ness.movie_release_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.ApiContextInitializer;
 
@@ -9,6 +10,7 @@ import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class MovieReleaseWebApplication {
     public static void main(String[] args) {
         // telegram api init
